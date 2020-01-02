0a15e046b6c2375403c5a235c1b0c86b972fe3aa
iqbalyarkhan <iqbal.khan@utdallas.edu>
2020-01-02 10:47:15 -0600

    Adds preliminary changes for about page

--- src/components/About.js
+++ src/components/About.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import config from "../../data/SiteConfig";
 
 const About = () => (
   <>
-    <h1>
-        Iqbal Khan
+    <h1 align="center">
+        {config.siteTitle}
     </h1>
     <h3>
           This page is a work in progress....
